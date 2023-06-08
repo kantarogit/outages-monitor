6b72a0f6cc27c922e17c0963a6ab270ec1563b68
kantarogit <kantarofilip@gmail.com>
2023-06-09 00:57:02 +0200

    debug secrets

--- client/mailClient.ts
+++ client/mailClient.ts
@@ -1,6 +1,9 @@
 import { OutagesPerUserModel } from "../model/outagesModel";
 const mailApiKey = process.env.MAIL_API_KEY;
 const mailApiSecret = process.env.MAIL_SECRET;
+console.log('envvvvs');
+console.log(process.env);
+
 
 const mailjet = require("node-mailjet").apiConnect(
   mailApiKey, mailApiSecret
