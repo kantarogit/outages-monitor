70c57086484500f58ded8072a2db8842e1f649d4
Filip Kantardjioski <kantarofilip@gmail.com>
2023-06-09 10:59:28 +0200

    Update evnClient.ts

--- client/evnClient.ts
+++ client/evnClient.ts
@@ -24,7 +24,9 @@ export async function getEvnOutages() {
       });
 }
 
-export function parseEvn(): Array<EvnModel> {
+export async function parseEvn(): Promise<Array<EvnModel>> {
+  await getEvnOutages();
+  
   const workbook = readFile('evn-data/evnOutages.xlsx');
   const sheetName = workbook.SheetNames[0];
   const sheet = workbook.Sheets[sheetName];
